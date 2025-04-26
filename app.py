673c0ecd97df64eb95235fac9e71826f19e8cc13
MD Mahbubur Rahman <85247674+mahbubtechsavvy@users.noreply.github.com>
2025-04-26 22:16:14 +0600

    Update app.py

--- app.py
+++ app.py
@@ -588,7 +588,7 @@ if st.session_state.view_data_clicked:
             </div>
             """
             # Now use the defined variable
-            st.markdown(components.html(elevenlabs_embed_code_only_widget, height=150))
+            components.html(elevenlabs_embed_code_only_widget, height=150)
             
         else:
             st.info("Waiting for AQI data...")
