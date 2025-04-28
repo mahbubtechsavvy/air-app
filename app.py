2a0155b2de8803a91624786a8f41f0f8e69fb945
MD Mahbubur Rahman <85247674+mahbubtechsavvy@users.noreply.github.com>
2025-04-28 18:28:03 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1186,7 +1186,7 @@ if st.session_state.view_data_clicked:
                 <script src="https://elevenlabs.io/convai-widget/index.js" async type="text/javascript"></script>
             </div>
             """
-            components.html(elevenlabs_embed_code_in_box, height=150)
+            components.html(elevenlabs_embed_code_in_box, height=350)
             
         else:
             st.info("Waiting for AQI data...")
